4eef9b8868963bf49cd3e382cd88e6aaf3cde2e4
Neeji <nagg844@gmail.com>
2019-08-02 16:29:21 +0530

    updated code and name of the fields

--- todo/todo_list/models.py
+++ todo/todo_list/models.py
@@ -3,8 +3,8 @@ from django.db import models
 # Create your models here.
 
 class List(models.Model):
-	item = models.CharField(max_length=200)
+	task = models.CharField(max_length=200)
 	completed = models.BooleanField(default=False)
 
 	def __str__(self):
-		return self.item + ' | ' + str(self.completed)
+		return self.task + ' | ' + str(self.completed)
--- todo/todo_list/views.py
+++ todo/todo_list/views.py
@@ -10,41 +10,41 @@ def home(request):
 
 		if form.is_valid():
 			form.save()
-			all_items = List.objects.all()
-			messages.success(request,("Items has been added to the List Successfully..."))
-			return render(request,'home.html',{'all_items':all_items})
+			all_tasks = List.objects.all()
+			messages.success(request,("task has been added to the List Successfully..."))
+			return render(request,'home.html',{'all_tasks':all_tasks})
 
 	else:
-		all_items = List.objects.all()
-		return render(request,'home.html',{'all_items':all_items})
+		all_tasks = List.objects.all()
+		return render(request,'home.html',{'all_tasks':all_tasks})
 
 def delete(request,list_id):
-	item = List.objects.get(pk=list_id)
-	item.delete()
-	messages.success(request,"Item has been deleted successfully...")
+	task = List.objects.get(pk=list_id)
+	task.delete()
+	messages.success(request,"task has been deleted successfully...")
 	return redirect("home")
 
 def cross_off(request,list_id):
-	item = List.objects.get(pk = list_id)
-	item.completed = True
-	item.save()
+	task = List.objects.get(pk = list_id)
+	task.completed = True
+	task.save()
 	return redirect("home")
 
 def uncross(request,list_id):
-	item = List.objects.get(pk=list_id)
-	item.completed=False
-	item.save()
+	task = List.objects.get(pk=list_id)
+	task.completed=False
+	task.save()
 	return redirect("home")
 
 def edit(request,list_id):
 	if request.method=='POST':
-		item = List.objects.get(pk = list_id)
-		form = ListForm(request.POST or None, instance=item)
+		task = List.objects.get(pk = list_id)
+		form = ListForm(request.POST or None, instance=task)
 
 		if form.is_valid():
 			form.save()
-			messages.success(request,('Item has been edited successfully...'))
+			messages.success(request,('task has been edited successfully...'))
 			return redirect('home')
 	else:
-		item = List.objects.get(pk=list_id)
-		return render(request,'edit.html',{'item':item})
+		task = List.objects.get(pk=list_id)
+		return render(request,'edit.html',{'task':task})
